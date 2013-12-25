ed1dbafdb088594d014ec88799bf600b3b9fe627
SkyKOG <akashdevaraju@gmail.com>
2013-12-25 23:15:45 +0530

    initialise app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickrfriends::Application.config.secret_token = 'b57048959c97e95874cfec938c1474ed794b34d0f9fb46b7449f724eb3665999e1018bcb0c8be4acfb1cfbe151c1203be5a92325e62321a17340cefa6f19c11d'
